ea3fdd673f68b21a1eb655567908b9dd6b4c3d05
KLessing <lessing@live.de>
2023-11-02 19:33:46 +0100

    Reuse validation test function + additional test cases

--- tests/validation_test.py
+++ tests/validation_test.py
@@ -8,22 +8,29 @@ CAPTION = "1 - 12"
 
 game_instance = Game(SCREEN_SIZE, PLAYER_NAMES, CAPTION)
 
-TEST_FIRST_MOVE = [
-    ([6, 1, 4, 3, 5, 2], True), # 3 x 7
-    ([6, 6, 6, 6, 6, 6] , True), # 6 x 6
-    ([6, 6, 6, 6] , True), # 4 x 6
-    ([6, 1, 4, 3] , True), # 2 x 7
-    ([6, 6, 6, 1, 4, 3] , False), # 2 x 7, 2 x 6
-    ([5, 5, 6, 4] , True),# 2 x 5 + 6 + 4 = 2 x 10 TODO
+MOVE_VALIDATION_TEST_CASES = [
+    # First Move Tests
+    ({}, [6, 1, 4, 3, 5, 2], True), # 3 x 7    
+    ({}, [6, 6, 6, 6, 6, 6] , True), # 6 x 6
+    ({}, [6, 6, 6, 6] , True), # 4 x 6
+    ({}, [6, 1, 4, 3] , True), # 2 x 7
+    ({}, [6, 6, 6, 1, 4, 3] , False), # 2 x 7, 2 x 6
+    # Continued Move Tests
+    ({1}, [2], False),
+    ({6, 6}, [6, 5], False),
+    ({6, 6}, [6], True),
+    ({6, 6}, [6, 6], True),
+    # TODO Keep Combination selection (Gets removed after first single selection)
+    ({5, 5}, [6, 4, 5, 5], True),
+    ({5, 5}, [5, 5, 6, 4], True),
+    ({5, 5}, [5, 4, 5, 6], True),
+    # TODO Enable selection of different combinations (when one combination is double dice)
+    ({}, [5, 5, 6, 4] , True), # 2 x 5 + 6 + 4 = 2 x 10
+    ({}, [4, 4, 6, 2] , True), # 2 x 4 + 6 + 2 = 2 x 8
+    ({}, [5, 3, 6, 2] , True), # Works
 ]
 
-# Test first moves with empty score
-@pytest.mark.parametrize("test_case, valid", TEST_FIRST_MOVE)
-def test_first_move(test_case: [int], valid: bool):
-    assert validate_selection(test_case, game_instance.scores[0], True) == valid
-
-def test_second_move():
-    game_instance.scores[0].set_selection([6, 6])
-    test_values = [6, 6]
-    assert validate_selection(test_values, game_instance.scores[0], False) == True
-
+@pytest.mark.parametrize("score, test_case, valid", MOVE_VALIDATION_TEST_CASES)
+def test_move_validations(score: set(), test_case: [int], valid: bool):
+    game_instance.scores[0].set_selection(score)
+    assert validate_selection(test_case, game_instance.scores[0], len(score) == 0) == valid
