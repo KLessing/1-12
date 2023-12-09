5ced8250dfc6439e3ef3be7926a1efd7740442ba
KLessing <lessing@live.de>
2023-12-09 03:46:46 +0100

    Fix Surfaces

--- animation/dice.py
+++ animation/dice.py
@@ -6,12 +6,12 @@ from OpenGL.GLU import *
 
 verticies = (  
     (-1, 1, 1), # 0: left top near
-    (1, 1, 1),
-    (1, -1, 1),
-    (-1, -1, 1),
+    (1, 1, 1), # 1: right top near
+    (1, -1, 1), # 2: right down near
+    (-1, -1, 1), # 3: left down near
     (-1, 1, -1), # 4: left top far
-    (1, 1, -1),
-    (1, -1, -1),
+    (1, 1, -1), # 5: right top far
+    (1, -1, -1), # 6: right down far
     (-1, -1, -1), # 7: left down far
 )
 
@@ -32,10 +32,10 @@ edges = (
 
 surfaces = (
     (0, 1, 2, 3),
-    (1, 2, 5, 6),
+    (1, 5, 6, 2),
     (4, 5, 6, 7),
     (4, 0, 3, 7),
-    (4, 5, 0, 1),
+    (4, 5, 1, 0),
     (7, 6, 2, 3)
 )
 
@@ -43,16 +43,16 @@ def Cube():
     glBegin(GL_QUADS)
     for surface in surfaces:
         x = 0
-        for vertex in surface:
+        for i, vertex in enumerate(surface):
             x+=1
             glColor3f(1, 1, 1)
+            glTexCoord2fv(textureCoordinates[i])
             glVertex3fv(verticies[vertex])
     glEnd()
 
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
-            glColor3f(0, 0, 0)
             glVertex3fv(verticies[vertex])
     glEnd()
 
