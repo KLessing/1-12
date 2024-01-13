035031f502aff7daed5661197ed72b7570304d06
KLessing <lessing@live.de>
2024-01-13 03:47:52 +0100

    Blender: WIP: Rotation Animation by script only

--- blender/blender_script.py
+++ blender/blender_script.py
@@ -1,7 +1,32 @@
 import bpy
+import math
+from math import radians
+
 
 dice = bpy.data.objects['Cube']
-dice.rotation_axis_angle = [90.0, 1.0, 0.0, 0.0]
 
-bpy.context.scene.render.filepath = ("C:\\Projects\\1-12\\blender\\images\\img")
-bpy.ops.render.render( animation=True, write_still=True ) 
+bpy.context.view_layer.objects.active = dice
+# TODO research select by name 
+bpy.ops.object.select_by_type(extend=False, type='MESH')
+
+#dice.rotation_axis_angle = [0.0, 0.0, 1.0, radians(90)]
+#dice.rotation_axis_angle = [45.0, 1.0, 0.0, 0.0]
+#dice.rotation_axis_angle = [180.0, 1.0, 0.0, 0.0]
+#dice.rotation_axis_angle = [1.0, 1.0, 1.0, 45.0]
+
+# insert keyframe at frame one
+start_frame = 1
+dice.keyframe_insert("rotation_euler", frame=start_frame)
+
+# change the rotation of the cube around z-axis
+degrees = 90
+radians = math.radians(degrees)
+dice.rotation_euler.z = radians
+
+# insert keyframe at the last frame
+end_frame = 180
+dice.keyframe_insert("rotation_euler", frame=end_frame)
+
+
+#bpy.context.scene.render.filepath = ("C:\\Projects\\1-12\\blender\\images\\img")
+#bpy.ops.render.render(animation=True, write_still=True)
