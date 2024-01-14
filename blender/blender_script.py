4ec90ab43fcf5f44930e25b74b046a3912d6bd23
KLessing <lessing@live.de>
2024-01-14 06:01:30 +0100

    Blender: Dynamic dice roll animation

--- blender/blender_script.py
+++ blender/blender_script.py
@@ -6,30 +6,39 @@ start_frame = 1
 end_frame = 180
 roll_count = 2
 
-# 180 x z = 1
-# 270 x y = 2
-# 270 x z = 3
-# 90 x z = 4
-# 90 x y = 5
-# 0 / 180 / 360 x y = 6 
+# Degree y z by index
+rolls = [
+    [180, False, True],
+    [270, True, False],
+    [270, False, True],
+    [90, False, True],
+    [90, True, False],
+    [180, True, False],
+]
 
-# reset rotations
-dice.rotation_euler.x = 0
-dice.rotation_euler.y = 0
-dice.rotation_euler.z = 0
-# insert keyframe at frame one
-dice.keyframe_insert("rotation_euler", frame=start_frame)
+def reset_rotation_values():
+    dice.rotation_euler.x = 0
+    dice.rotation_euler.y = 0
+    dice.rotation_euler.z = 0
+    # insert keyframe at frame one
+    dice.keyframe_insert("rotation_euler", frame=start_frame)
 
-# change the rotation of the cube
-# (negative to roll "away")
-degrees = 270 + (360 * roll_count)
-radians = math.radians(degrees)
-dice.rotation_euler.x = -radians
-#dice.rotation_euler.y = -radians
-dice.rotation_euler.z = -radians
+def animate_roll(degree: int, count: int, y: bool, z: bool):
+    # change the rotation of the cube
+    # (negative to roll "away")
+    degrees = degree + (360 * count)
+    radians = math.radians(degrees)
 
-# insert keyframe at the last frame
-dice.keyframe_insert("rotation_euler", frame=end_frame)
+    dice.rotation_euler.x = -radians
+    dice.rotation_euler.y = -radians if y else 0
+    dice.rotation_euler.z = -radians if z else 0
+
+    # insert keyframe at the last frame
+    dice.keyframe_insert("rotation_euler", frame=end_frame)
+
+i = 5
+reset_rotation_values()
+animate_roll(rolls[i][0], roll_count, rolls[i][1], rolls[i][2])
 
 # generate images from animation
 # TODO set Frame Range Set in Camera object Accordingly
