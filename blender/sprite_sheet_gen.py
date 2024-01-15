7bb33083bb586157a9008c98e9caafb2739296fb
KLessing <lessing@live.de>
2024-01-15 01:13:05 +0100

    Sprite Generation: Generate all 6 outcomes per row in one sprite sheet

--- blender/sprite_sheet_gen.py
+++ blender/sprite_sheet_gen.py
@@ -1,26 +1,25 @@
 from PIL import Image
 import os
 
-def add_img(sprite: Image, img: Image, col: int):
-    w = sprite.size[0] + img.size[0]
-    h = 240 * col
-    im = Image.new("RGBA", (w, h))
+def add_img(sprite: Image, img: Image, y: int, x: int):
+    w = 240 * x
+    h = 240 * (y - 1)
+    im = Image.new("RGBA", (240 * 16, 240 * 6)) # Full Resolution
   
     im.paste(sprite)
-    im.paste(img, (sprite.size[0], 0))
+    im.paste(img, (w, h))
 
     return im
+
+sprite_sheet = Image.new("RGBA", (0, 0))
 
 for i in range(1, 7):
     dir = os.getcwd() + "\\images\\" + str(i)
     files = os.listdir(dir)
 
-    # start with first img
-    sprite_sheet = Image.open(dir + "\\" + files[0])
-
-    for i in range(1, len(files)):
-        file = dir + "\\" + files[i]
+    for j in range(len(files)):
+        file = dir + "\\" + files[j]
         img = Image.open(file)
-        sprite_sheet = add_img(sprite_sheet, img, 1)
+        sprite_sheet = add_img(sprite_sheet, img, i, j)
 
 sprite_sheet.save("sprite_sheet.png")
