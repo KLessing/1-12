98cab26b01da3a9463dc258c9e3a773a9e484766
KLessing <lessing@live.de>
2024-02-18 14:46:48 +0100

    Cleanup DrawOnlyDice Objects

--- src/dice_controller.py
+++ src/dice_controller.py
@@ -19,23 +19,25 @@ class DiceController():
 			self.dice_imgs[i] = sprite_sheet.get_roll_imgs(i)
 			self.selected_dice_img[i] = sprite_sheet.get_dice_img(i, True)
 
+	# roll the new current dice, start animation and return dice
 	def roll_dice(self, count: int, screen_size):
 		dice = []
 		for i in range(0, count):
 			# roll dice (1-6)
 			rdm = random.randrange(1, globals.MAX_DICE_COUNT + 1)
 			delay = random.randrange(5, 10)
-			dice.append(Dice(self.dice_imgs[rdm], self.selected_dice_img[rdm], rdm, self.get_pos(i, count, screen_size, globals.IMG_SIZE), delay))
+			dice.append(Dice(self.dice_imgs[rdm], self.selected_dice_img[rdm], rdm, self.get_mid_pos(i, count, screen_size, globals.IMG_SIZE), delay))
 		return dice
 
 	def get_used_dice(self, values: [int]):
 		dice = []
+		width = globals.IMG_SIZE * 0.5
 		for index, value in enumerate(values):
-			dice.append(DrawOnlyDice(self.selected_dice_img[value], value, index, 0.5))
+			dice.append(DrawOnlyDice(self.selected_dice_img[value], self.get_top_pos(index, width), value, width))
 		return dice
 
 	# calc dice pos dynamically in screen mid according to dice index and count of all dice
-	def get_pos(self, index: int, count: int, screen_size: tuple, width: int) -> tuple:
+	def get_mid_pos(self, index: int, count: int, screen_size: tuple, width: int) -> tuple:
 		# separate all dice into two rows
 		# round down to whole number with //
 		separator = count // 2 
@@ -62,3 +64,11 @@ class DiceController():
 		x = col_index * (width + OFFSET) + dice_row_pos
 			
 		return (x, y)
+
+	# calc pos for two rows in top left corner
+	def get_top_pos(self, index: int, width: int) -> tuple:
+		y = OFFSET
+		if index >= 3:
+			y += width + OFFSET
+			index -= 3
+		return (index * width + (index + 1) * OFFSET , y)
